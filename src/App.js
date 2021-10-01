9b372f1488b234e3163f31929d0a3eac750c0469
yijingw37 <31359897+yijingw37@users.noreply.github.com>
2021-10-01 11:57:58 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
 import "./App.css";
 import CourseList from "./components/CourseList";
 import { useData } from "./utilities/firebase.js";
-
-const meetsPat =
-  /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
-
-const timeParts = (meets) => {
-  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-  return !match
-    ? {}
-    : {
-        days,
-        hours: {
-          start: hh1 * 60 + mm1 * 1,
-          end: hh2 * 60 + mm2 * 1,
-        },
-      };
-};
-
-const mapValues = (fn, obj) =>
-  Object.fromEntries(
-    Object.entries(obj).map(([key, value]) => [key, fn(value)])
-  );
-
-const addCourseTimes = (course) => ({
-  ...course,
-  ...timeParts(course.meets),
-});
-
-const addScheduleTimes = (schedule) => ({
-  title: schedule.title,
-  courses: mapValues(addCourseTimes, schedule.courses),
-});
+import { addScheduleTimes } from "./utilities/time";
 
 const Banner = ({ title }) => <h1>{title}</h1>;
 
--- src/utilities/time.js
+++ src/utilities/time.js
@@ -20,3 +20,34 @@ const courseConflict = (course1, course2) =>
 export const terms = { F: "Fall", W: "Winter", S: "Spring" };
 
 export const getCourseTerm = (course) => terms[course.id.charAt(0)];
+
+const meetsPat =
+  /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = (meets) => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match
+    ? {}
+    : {
+        days,
+        hours: {
+          start: hh1 * 60 + mm1 * 1,
+          end: hh2 * 60 + mm2 * 1,
+        },
+      };
+};
+
+const mapValues = (fn, obj) =>
+  Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, fn(value)])
+  );
+
+const addCourseTimes = (course) => ({
+  ...course,
+  ...timeParts(course.meets),
+});
+
+export const addScheduleTimes = (schedule) => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses),
+});
